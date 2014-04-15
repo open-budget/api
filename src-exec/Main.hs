d2c52fbf77e7ee17c1ee9d0852c1dc60966cfb2e
Sergey Gerasimov <mail@grsmv.com>
2014-04-15 18:18:20 +0300

    Web interface: work in progress

--- src-exec/Main.hs
+++ src-exec/Main.hs
@@ -5,18 +5,28 @@ module Main where
 import           OpenBudget.Builder
 import           OpenBudget.Types.Database
 import           Web.Scotty
+import           Paths_open_budget_database (getDataFileName)
+import           Data.Aeson (encode)
 
 main :: IO ()
 main = do
     db <- newDatabase
+    doc <- getDataFileName "resourses/index.html"
 
     scotty 3000 $ do
 
-        get "/areas.json" $
-            json (areas db)
+        get "/" $
+            file doc
 
-        get "/documents.json" $
-            json (documents db)
+        get "/v1/areas.json" $
+            jsonUtf8 (areas db)
 
-        get "/expenses.json" $
-            json (expenses db)
+        get "/v1/documents.json" $
+            jsonUtf8 (documents db)
+
+        get "/v1/expenses.json" $
+            jsonUtf8 (expenses db)
+
+        where jsonUtf8 xs = do
+                setHeader "Content-Type" "application/json; charset=utf-8"
+                raw $ encode xs
